7419f4e5f0b8deb13c4e5f930ea5d9887cd082d8
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-11-15 12:44:45 +0300

    Добавил в функцию цикл

--- Lab_55/A1.py
+++ Lab_55/A1.py
@@ -1,7 +1,12 @@
-def shortener(text):
-    position1 = text.rfind('(')
-    position2 = text.rfind(')')
-    text = text.replace(text[position1:position2 + 1], '')
+def function(text):
+    while '(' in text:
+        position1 = text.find('(')
+        position2 = text.find(')', position1)
+        if position2 != 1:
+            text = text.replace(text[position1:position2 + 1], '')
+        else:
+            text = text[:position1]
     return text
+
 text = str(input())
-print(shortener(text))
+print(function(text))
